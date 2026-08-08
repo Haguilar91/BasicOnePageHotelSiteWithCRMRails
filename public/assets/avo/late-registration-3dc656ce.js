8baf3138f747416faea62253b5826a6a59c47036
Hector Aguilar <haguilar91@gmail.com>
2026-08-08 00:58:47 -0600

    Initial

---
+++ public/assets/avo/late-registration-3dc656ce.js
@@ -0,0 +1,2 @@
+(()=>{var n=window.Stimulus;window.Avo.configuration.stimulus_controllers&&window.Avo.configuration.stimulus_controllers.forEach(([o,i])=>{n.register(o,window[i])});})();
+//# sourceMappingURL=/assets/avo/late-registration-aa106267.js.map
